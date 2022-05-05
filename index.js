70ee370ba62a55047ca09311dd4ccc4db72ae47f
Zane Stock <zanestock3@gmail.com>
2022-05-05 08:35:15 -0500

    commit tree eac5f8688a23b9e1f5d85c830e0bd06032538f9e

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function createEmployeeRecord(employee) {
+    
+    return {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(event) {
+    let [date, hour] = event.split(" ")
+    let eventObj = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    }
+    
+    this.timeInEvents.push(eventObj)
+    return this
+}
+
+function createTimeOutEvent(event) {
+    let [date, hour] = event.split(" ")
+    let eventObj = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    }
+    
+    this.timeOutEvents.push(eventObj)
+    return this
+}
